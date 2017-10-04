6a029b5cd8c82d7a2df48b731fc45ac81af9bcc3
wangzemin <1966199652@qq.com>
2017-10-04 22:19:36 +0800

    modify the my.js file, and change the number from % to Int

--- my.js
+++ my.js
@@ -5,49 +5,49 @@
 		var btn = btns.children;
 		var styleList = [
 		{
-			"width": "70%",
-			"height": "80%",
-			"left": "15%",
-			"top": "18%",
-			"backgroundColor": "pink",
-			"opacity": 1,
-			"z-index": 1
+			width: 800,
+			height: 500,
+			left: 200,
+			top: 100,
+			backgroundColor: pink,
+			opacity: 100,
+			z-index: 4
 		},
 		{
-			"width": "40%",
-			"height": "50%",
-			"left": "55%",
-			"top": "10%",
-			"backgroundColor": "lightblue",
-			"opacity": 0.5,
-			"z-index": 0
+			width: 600,
+			height: 375,
+			left: 600,
+			top: 70,
+			backgroundColor: lightblue,
+			opacity: 50,
+			z-index: 3
 		},
 		{
-			"width": "20%",
-			"height": "25%",
-			"left": "60%",
-			"top": "2%",
-			"backgroundColor": "lightgreen",
-			"opacity": 0.2,
-			"z-index": -1
+			width: 400,
+			height: 250,
+			left: 750,
+			top: 20,
+			backgroundColor: lightgreen,
+			opacity: 20,
+			z-index: 2
 		},
 		{
-			"width": "20%",
-			"height": "25%",
-			"left": "20%",
-			"top": "2%",
-			"backgroundColor": "orange",
-			"opacity": 0.2,
-			"z-index": -1
+			width: 400,
+			height: 250,
+			left: 50,
+			top: 20,
+			backgroundColor: orange,
+			opacity: 20,
+			z-index: 2
 		},
 		{
-			"width": "40%",
-			"height": "50%",
-			"left": "5%",
-			"top": "10%",
-			"backgroundColor": "purple",
-			"opacity": 0.5,
-			"z-index": 0
+			width: 600,
+			height: 375,
+			left: 0,
+			top: 70,
+			backgroundColor: purple,
+			opacity: 50,
+			z-index: 3
 		}];
 		for(var i=0; i<5; i++){
 			for(var k in styleList[i]){
@@ -62,28 +62,39 @@
 			styleList.unshift(styleList.pop());
 			setStyle(divs, styleList);
 		});
-		function setStyle(obj, attrs){
-			for(var attr in attrs[i]){
-				if(attr == 'opacity' || attr == 'zIndex'){
-					var nowVal = getStyle(obj[i], attr) * 100;
-				}else{
-					var nowVal = parseInt(getStyle(obj[i], attr));
-				}
-				var value = attrs[i][attr];
-				var step = (value - nowVal) / 10;
-				step = step ? Math.ceil(step) : Math.floor(step);
-				obj[i].timer = setInterval(function(){
-					nowVal = nowVal + step;
-					if(attr == 'opacity' || attr == 'zIndex'){
-						obj[i].style[attr] = nowVal / 100;
+		function setStyle(obj, attrs, fn){
+			clearInterval(obj.timer);
+			obj.timer = setInterval(function(){
+				var flag = true;
+				for(var k in attrs){
+					var nowVal = 0;
+					if(k === 'opacity'){
+						nowVal = Math.round(getStyle(obj,k) * 100) || 100;
 					}else{
-						obj[i].style[attr] = nowVal + 'px';
+						nowVal = parseInt(getStyle(obj,k)) || 0;
 					}
-					if(value == nowVal){
-						clearInterval(obj[i].timer);
+					var step = (attrs[k] - nowVal) / 10;
+					step = step > 0 ? Math.ceil(step) : Math.floor(step);
+					nowVal = nowVal + step;
+					if(k === 'opacity'){
+						obj.style.opacity = nowVal / 100;
+						obj.style.filter = 'alpha(opacity =' + nowVal +');'
+					}else if(k === 'zIndex'){
+						obj.style.zIndex = attrs[k];
+					}else{
+						obj.style[k] = nowVal + 'px';
 					}
-				}, 30);
-			}
+					if(attrs[k] != nowVal){
+						flag = false;
+					}
+				}
+				if(flag){
+					clearInterval(obj.timer);
+					if(fn){
+						fn();
+					}
+				}
+			}, 30);
 		}
 		function getStyle(obj, attr){
 			if(obj.currentStyle){
